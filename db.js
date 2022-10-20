11c0cab7441f29a0120ce17a5c29568d3b97df15
MatiasBlanco0 <101400282+MatiasBlanco0@users.noreply.github.com>
2022-10-20 15:27:25 -0300

    feat(db): connection settngs

--- db.js
+++ db.js
@@ -2,6 +2,10 @@ const mysql = require("mysql2");
 
 const conn = mysql.createConnection({
     // Completar con los datos de la conexión
+    host:"localhost",
+    user: 'root',
+    password: 'rootroot',
+    database: 'spoticfy'
 });
 
 module.exports = conn;
